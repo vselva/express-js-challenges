c43a3d7f5c048b193734856690879ebfb1c95ae4
Selvakumar <vselva@gmail.com>
2025-04-30 15:21:55 +0530

    Added Readm

--- 4-jwt-auth-server/middleware/auth.js
+++ 4-jwt-auth-server/middleware/auth.js
@@ -5,11 +5,9 @@ const JWT_SECRET = process.env.JWT_SECRET;
 // middlware to check authenticate token
 function authenticateToken(req, res, next) {
     // Get the header from 'authorization'
-    const authHeader = req.headers['authorization'];
-    
-    // 
+    const authHeader = req.headers['authorization']; 
     const token = authHeader && authHeader.split(' ')[1];
-
+    
     // if no token return 'token missing'
     if (!token)
         return res.status(404).json({message: 'token missing'});
